fe7043f8f7ee2b41eb8188af067baba614a7e104
gaudy540octopus <93523996+gaudy540octopus@users.noreply.github.com>
2022-10-10 00:14:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const FoodModel = require("./models/Food");
-
-app.use(express.json());
-
-mongoose.connect{
-  "mongodb+srv://newuser:password12345@crud.fd9xk.mongodb.net/food?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-  }
-};
-
-app.get("/", async (req, res) => {
-  const food = new FoodeModel({foodName: "Apple", daysSinceIAte: 3});
-
-  try {
-    await food.save();
-
-  } catch (err) {
-    console.log(err);
-
-  } 
-
-});
-
-app.listen(3001, ()=> {
-  console.log('Server running on port 3001...')
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
